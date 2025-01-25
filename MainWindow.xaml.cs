7e7ae3371262e655ca74a173b03a92e327fc3a68
Alexandre Grosjean <apj.grosjean@gmail.com>
2025-01-25 19:07:40 +0100

    creation of secondary branch

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,7 +5,7 @@ namespace Solfege_Theory
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
-    /// 
+
     public partial class MainWindow : Window
     {
         public MainWindow()
