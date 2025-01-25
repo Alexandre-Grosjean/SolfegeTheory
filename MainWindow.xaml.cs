15350a05a10101345682fe863e9ae572e6b00d19
Alexandre Grosjean <apj.grosjean@gmail.com>
2025-01-25 19:00:22 +0100

    project creation

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Solfege_Theory
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
